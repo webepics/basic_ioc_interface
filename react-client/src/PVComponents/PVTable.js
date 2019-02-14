1b071a4c8024dbf54f7511172836084353110e45
Rosemary Waghorn <flyladyhawke@gmail.com>
2019-02-14 15:10:27 +1100

    Add toggle checkbox / LED

--- react-client/src/PVComponents/PVTable.js
+++ react-client/src/PVComponents/PVTable.js
@@ -5,16 +5,20 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import PVComponent, { ConnectComponent } from '../PVComponent'
-import PVCheckbox from './PVCheckbox'
+import PVCheckbox from './PVCheckbox';
+import PVLed from './PVLed';
 
 class PVTable extends PVComponent {
-
     ids = []
-
     constructor(props) {
         super(props)
+        this.state = {
+            displayType: 'checkbox'
+        };
+        this.toggleDisplay = this.toggleDisplay.bind(this)
         for (let card = 1; card <= props.cardCount; card++) {
             let row = Array(props.pCount).fill(0)
             row = row.map((_, p) => {
@@ -26,6 +30,12 @@ class PVTable extends PVComponent {
         }
     }
 
+    toggleDisplay() {
+        this.setState({
+            displayType: this.state.displayType === 'checkbox' ? 'led' : 'checkbox',
+        })
+    }
+
     render() {
         const rows = this.ids.map((item, i) => {
             return (
@@ -35,15 +45,28 @@ class PVTable extends PVComponent {
                             { i + 1 }
                         </Typography>
                     </TableCell>
-                    {item.map((element, j) => <TableCell style={{padding:0}} key={element}> <PVCheckbox pv={element} /></TableCell>)}
+                    {item.map((element, j) =>
+                        <TableCell style={{padding:0}} key={element}>
+                            {this.state.displayType === 'checkbox' ?
+                            <PVCheckbox pv={element} /> :
+                            <PVLed pv={element} />
+                            }
+                        </TableCell>)}
                 </TableRow>
             );
         });
-        return <Table>
+        return (
+        <React.Fragment>
+        <br/><br/>
+        <Button variant="outlined" color="primary" onClick={this.toggleDisplay}>Toggle Checkbox / LED</Button>
+        <br/>
+        <Table>
             <TableBody>
                 {rows}
             </TableBody>
         </Table>
+        </React.Fragment>
+        )
     }
 }
 
