3e5a09177a59c9261d0a4dbbae689ba5d8473b28
Craig Haskins <Craig.Haskins@csiro.au>
2019-03-13 23:17:49 +0800

    PV bar graph initial commit

--- react-client/src/components/ExampleTable.js
+++ react-client/src/components/ExampleTable.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import PVTable from '../PVComponents/PVTable'
+import PVBarGraph from '../PVComponents/PVBarGraph'
 
 class ExampleTable extends Component {
   render() {
     return (
         <React.Fragment>
-            <PVTable template='$(p)$(card)BFStatus:fpgaConfigured' prefix='ak' pCount={36} cardCount={12} />
+            <PVBarGraph template='$(p):paf:ctrl:adc:pafAvTemp' macro="ak{01..36}"  />
         </React.Fragment>
     );
   }
--- react-client/src/routes/index.js
+++ react-client/src/routes/index.js
@@ -1,7 +1,9 @@
 import ExampleTable from '../components/ExampleTable';
+import ExampleBarGraph from '../components/ExampleBarGraph';
 
 var routes = [
     { path: "/", exact:true, component: ExampleTable },
+    { path: "/bargraph", exact:true, component: ExampleBarGraph },
 ]
 
 export default routes;
