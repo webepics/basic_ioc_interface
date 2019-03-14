bcf55e3992bd14c27fccae873cc3243a0cce1454
Troup, Euan (CASS, Marsfield) <Euan.Troup@csiro.au>
2019-03-14 16:40:10 +1100

    get PV with  timestamp, status, severity from metadata

--- web_put/src/webput/app.py
+++ web_put/src/webput/app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from caproto.sync import client
 from flask import Flask, make_response, request
+import time
+import json
 
 
 app = Flask(__name__)
@@ -12,3 +14,27 @@ def put():
     response.headers["Access-Control-Allow-Origin"] = "*"
     client.write(request.args.get("pv"), request.args.get("value"))
     return response
+
+@app.route("/get", methods=["GET"])
+def get_channels_status():
+    ''' process a list of pv names to get their metadata
+    ''' 
+    pvs = []
+    chanNames = request.args.get('channels')
+    if chanNames is not None:
+        myNames = chanNames.split(',')
+    else:
+        return make_response("Error: no names")
+
+    for name in myNames:
+        pvInfo = {}
+        #get timestamp, severity
+        val = client.read(name, data_type='time')
+        pvInfo['value'] = val.data.tolist()
+        pvInfo['timestamp'] = val.metadata.timestamp
+        pvInfo['timestring'] = time.asctime(time.gmtime(pvInfo['timestamp']))
+        pvInfo['status'] = val.metadata.status
+        pvInfo['severity'] = val.metadata.severity
+        pvs.append(pvInfo)
+    allValues = {'values': pvs}
+    return make_response(json.dumps(allValues))
