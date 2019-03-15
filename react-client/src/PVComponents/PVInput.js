bfe7bf660cdfbcceeda87b208534af1b40623d36
Rosemary Waghorn <flyladyhawke@gmail.com>
2019-03-15 12:15:18 +1100

    Use material ui for dialog
    Fix bugs in logic

--- react-client/src/PVComponents/PVInput.js
+++ react-client/src/PVComponents/PVInput.js
@@ -1,64 +1,82 @@
 
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
-import Modal from '@material-ui/core/Modal';
 
+import DiscardDialog from '../components/DiscardDialog'
 import PVComponent, { ConnectComponent } from '../PVComponent'
 
+// TODO Move the dialog to the PV Table level ?
 class PVInput extends PVComponent {
-
     constructor(props) {
         super(props)
-        this.state = { textValue: false, modalOpen: false };
+        this.state = { textValue: false, dialogOpen: false };
         this.handleChange = this.handleChange.bind(this)
         this.handleBlur = this.handleBlur.bind(this)
         this.handleKeyPress = this.handleKeyPress.bind(this)
+        this.handleDialogClose = this.handleDialogClose.bind(this)
+        this.handleDialogDiscard = this.handleDialogDiscard.bind(this)
+    }
+
+    handleDialogClose = () => {
+        this.setState({ dialogOpen: false });
+    }
+
+    handleDialogDiscard = () => {
+        this.setState({textValue: this.getPvValue(), dialogOpen: false })
     }
 
     handleChange(event) {
         event.persist()
         const val = event.target.value;
-        this.setState({textValue:val})
+        this.setState({textValue: val})
         this.forceUpdate();
     }
-    handleBlur(event) {
-        const {pvs, pv} = this.props;
-        const pvValue = pvs[pv] && pvs[pv].value ? pvs[pv].value : "";
-        if (this.state.textValue !== false && pvValue !== this.state.textValue) {
-            //this.setState({modalOpen: true})
-            if (window.confirm('Do you mean to leave?') === true) {
-                this.state.textValue = pvValue;
-                this.forceUpdate();
 
-            }
+    handleBlur(event) {
+        if (this.isValueNotMatch()) {
+            this.setState({dialogOpen: true})
         }
     }
+
     handleKeyPress(event) {
         if (event.key === 'Enter') {
             this.write(this.props.pv, this.state.textValue)()
         }
     }
-    checked() {
-        return (this.props.pvs[this.props.pv] && this.props.pvs[this.props.pv].value && this.props.pvs[this.props.pv].value !== 0) ? true : false
+
+    getPvValue() {
+        const {pvs, pv} = this.props;
+        return pvs[pv] && pvs[pv].value ? pvs[pv].value : "";
+    }
+
+    isValueNotMatch() {
+        // TODO make this work for non int checks as well
+        return this.state.textValue !== false && this.getPvValue() !== parseInt(this.state.textValue)
     }
 
     render() {
-        const {pvs, pv} = this.props;
-        const pvValue = pvs[pv] && pvs[pv].value ? pvs[pv].value : "";
+        const {pv} = this.props;
+        const pvValue = this.getPvValue()
         const title = `pv: ${pv} \nvalue: ${pvValue}`;
-        const color = this.state.textValue !== false && pvValue !== this.state.textValue ? 'yellow' : '#90EE90';
-        const val = this.state.textValue !== false && pvValue !== this.state.textValue ? this.state.textValue : pvValue;
+        const color = this.isValueNotMatch() ? 'yellow' : '#90EE90';
+        const val = this.isValueNotMatch() ? this.state.textValue : pvValue;
         return (
         <React.Fragment>
-        <TextField color='primary' title={title} style={{padding:"4px",backgroundColor:color}} value={val} onBlur={this.handleBlur} onKeyPress={this.handleKeyPress} onChange={this.handleChange}/>
-        <Modal
-            open={this.state.modalOpen}
-            >
-            <div>Do you want to discard your value?</div>
-        </Modal>
+        <TextField
+            color='primary'
+            title={title}
+            style={{padding:"4px",backgroundColor:color}}
+            value={val}
+            onBlur={this.handleBlur}
+            onKeyPress={this.handleKeyPress}
+            onChange={this.handleChange}/>
+        <DiscardDialog
+            open={this.state.dialogOpen}
+            handleClose={this.handleDialogClose}
+            handleDiscard={this.handleDialogDiscard}
+        />
         </React.Fragment>
         )
-
     }
 }
 
